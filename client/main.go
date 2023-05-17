a4343a69317e0235e7fc411ee1a39827ef91bf75
Garrett Graves <gravesg57@gmail.com>
2023-05-17 15:04:32 -0700

    fix go mod

--- client/main.go
+++ client/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/url"
+	"os"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	"github.com/pion/webrtc/v3"
@@ -56,7 +58,51 @@ func main() {
 	}
 	defer c.Close()
 
-	onIce := func(candidate *webrtc.ICECandidate) {
+	conn := newConn(c)
+
+	go func() {
+		for {
+			_, message, err := conn.ws.ReadMessage()
+			if err != nil {
+				log.Printf("err reading message %+v", err)
+			}
+
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	// Set the handler for Peer connection state
+	// This will notify you when the peer has connected/disconnected
+
+}
+
+// FIXME add to new file
+
+type Conn struct {
+	ws                *websocket.Conn
+	conn              *webrtc.PeerConnection
+	pendingCandidates []*webrtc.ICECandidate
+	mu                sync.Mutex
+}
+
+func newConn(ws *websocket.Conn) *Conn {
+	// Prepare the configuration
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		log.Fatal("pc err", err)
+	}
+
+	// When an ICE candidate is available send to the other Pion instance
+	// the other Pion instance will add this candidate by calling AddICECandidate
+	peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
 		if candidate == nil {
 			return
 		}
@@ -74,14 +120,79 @@ func main() {
 			log.Printf("Error marshaling message to json %+v", msg, err)
 			return
 		}
-		if err := c.WriteMessage(websocket.TextMessage, payload); err != nil {
+		if err := ws.WriteMessage(websocket.TextMessage, payload); err != nil {
 			log.Printf("Error sending websocket message %+v err %+v", msg, err)
+		}
+	})
+
+	peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		log.Printf("Peer Connection State has changed: %s\n", s.String())
+
+		if s == webrtc.PeerConnectionStateFailed {
+			// Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
+			// Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+			// Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+			log.Println("Peer Connection has gone to failed exiting")
+			os.Exit(0)
+		}
+	})
+
+	peerConnection.OnTrack(func(t *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+		kind := "unknown kind"
+		if t.Kind() == webrtc.RTPCodecTypeVideo {
+			kind = "video"
+		} else if t.Kind() == webrtc.RTPCodecTypeAudio {
+			kind = "audio"
+		}
+		log.Printf("got track %s", kind)
+	})
+
+	return &Conn{
+		ws:                ws,
+		conn:              peerConnection,
+		pendingCandidates: make([]*webrtc.ICECandidate, 0),
+	}
+
+	// defer func() {
+	// 	if err := peerConnection.Close(); err != nil {
+	// 		fmt.Printf("cannot close peerConnection: %v\n", err)
+	// 	}
+	// }()
+}
+
+func (c *Conn) Offer(offer webrtc.SessionDescription) error {
+	return c.conn.SetRemoteDescription(offer)
+}
+
+func (c *Conn) Answer() (*webrtc.SessionDescription, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	answer, err := c.conn.CreateAnswer(nil)
+	if err != nil {
+		return nil, err
+	}
+	if err = c.conn.SetLocalDescription(answer); err != nil {
+		return nil, err
+	}
+
+	for _, candidate := range c.pendingCandidates {
+		if err = c.conn.AddICECandidate(webrtc.ICECandidateInit{Candidate: candidate.String()}); err != nil {
+			log.Printf("error adding ice candidate %s %+v", candidate.String(), err)
 		}
 	}
 
-	_ = NewConnection(onIce)
+	return &answer, nil
+}
 
-	// Set the handler for Peer connection state
-	// This will notify you when the peer has connected/disconnected
+func (c *Conn) AddIceCandidate(candidate *webrtc.ICECandidate) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
+	if c.conn.RemoteDescription() == nil {
+		c.pendingCandidates = append(c.pendingCandidates, candidate)
+		return nil
+	} else {
+		return c.conn.AddICECandidate(webrtc.ICECandidateInit{Candidate: candidate.String()})
+	}
 }
