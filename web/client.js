17e911ffb5f86ab85fed00215ba470d25d45b272
TryAndErr0r <adrian.mehmke@gmail.com>
2023-05-31 07:08:39 +0200

    done

--- web/client.js
+++ web/client.js
@@ -1,4 +1,4 @@
-import { generateRandomString } from "./util.js";
+import { generateRandomString, decodeDatachannelMessage } from "./util.js";
 
 export class Client {
   constructor(stream, noPub, noSub, room, useDockerWs) {
@@ -53,6 +53,15 @@ export class Client {
 
       this.sub.ontrack = (e) => {
         console.log("houston we have a track", e);
+      };
+      this.sub.ondatachannel = (e) => {
+        const { channel } = e;
+        console.log("got chan", channel);
+        if (channel.label === "transcriptions") {
+          channel.onmessage = (msg) => {
+            decodeDatachannelMessage(msg.data);
+          };
+        }
       };
     }
   }
