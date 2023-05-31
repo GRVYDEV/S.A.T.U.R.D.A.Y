af35f11edb929b6d1b79c3aa02370ac6c67a822e
Garrett Graves <gravesg57@gmail.com>
2023-05-30 17:56:17 -0700

    start adding media selectors to web, add hacky segment filtering to whisper engine

--- web/media.js
+++ web/media.js
@@ -8,3 +8,36 @@ export async function getMedia() {
     console.error("error getting media", err.message);
   }
 }
+
+export async function getDevices() {
+  await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter((d) => d.kind === "videoinput");
+  const audioDevices = devices.filter((d) => d.kind === "audioinput");
+  return { audioDevices, videoDevices };
+}
+
+export async function getCamera(deviceId) {
+  let media;
+  const videoConstraints = {
+    deviceId: deviceId ? { exact: deviceId } : null,
+  };
+
+  return await navigator.mediaDevices.getUserMedia({
+    video: videoConstraints,
+    audio: false,
+  });
+}
+
+export async function getMic(deviceId) {
+  let media;
+  const audioConstraints = {
+    deviceId: deviceId ? { exact: deviceId } : null,
+    noiseSuppression: true,
+  };
+
+  return await navigator.mediaDevices.getUserMedia({
+    video: false,
+    audio: audioConstraints,
+  });
+}
