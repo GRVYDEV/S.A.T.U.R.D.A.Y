be692d6f82253d857f6692cf4fb1bbc628fa01cb
Garrett Graves <gravesg57@gmail.com>
2023-06-05 16:41:10 -0700

    move to slog

--- log/go.mod
+++ log/go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/rs/zerolog v1.29.1 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
--- log/log.go
+++ log/log.go
@@ -1,139 +1,97 @@
 package log
 
 import (
+	"context"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
+	"time"
 
-	"github.com/go-logr/logr"
-	"github.com/go-logr/zerologr"
-	"github.com/rs/zerolog"
+	"golang.org/x/exp/slog"
 )
 
-const (
-	timeFormat = "2006-01-02 15:04:05.000"
-)
+var level = new(slog.LevelVar)
 
-// GlobalConfig config contains global options
-type GlobalConfig struct {
-	V int `mapstructure:"v"`
-}
-
-// SetGlobalOptions sets the global options, like level against which all info logs will be
-// compared.  If this is greater than or equal to the "V" of the logger, the
-// message will be logged. Concurrent-safe.
-func SetGlobalOptions(config GlobalConfig) {
-	lvl := 1 - config.V
-	if v := int(zerolog.TraceLevel); lvl < v {
-		lvl = v
-	} else if v := int(zerolog.InfoLevel); lvl > v {
-		lvl = v
-	}
-	zerolog.SetGlobalLevel(zerolog.Level(lvl))
-}
-
-// SetVLevelByStringGlobal does the same as SetGlobalOptions but
-// trying to expose verbosity level as more familiar "word-based" log levels
-func SetVLevelByStringGlobal(level string) {
-	if v, err := zerolog.ParseLevel(level); err == nil {
-		zerolog.SetGlobalLevel(v)
-	}
+func SetLevel(lv slog.Level) {
+	level.Set(lv)
 }
 
 type Logger struct {
-	logr.Logger
+	*slog.Logger
 }
 
-func (l Logger) Debug(msg string, keysAndValues ...interface{}) {
-	l.Logger.V(1).Info(msg, keysAndValues...)
+func New() Logger {
+	replace := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.SourceKey {
+			source := a.Value.Any().(*slog.Source)
+			source.File = filepath.Base(source.File)
+		}
+
+		return a
+	}
+	return Logger{
+		slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource:   true,
+			ReplaceAttr: replace,
+			Level:       level,
+		})),
+	}
+}
+
+func (l Logger) logf(level slog.Level, format string, v ...any) {
+	if !l.Enabled(context.Background(), level) {
+		return
+	}
+
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:])
+	r := slog.NewRecord(time.Now(), level, fmt.Sprintf(format, v...), pcs[0])
+	_ = l.Handler().Handle(context.Background(), r)
+}
+
+func (l Logger) log(level slog.Level, msg string, args ...any) {
+	if !l.Enabled(context.Background(), level) {
+		return
+	}
+
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:])
+	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
+	r.Add(args...)
+	_ = l.Handler().Handle(context.Background(), r)
 }
 
 func (l Logger) Debugf(format string, v ...any) {
-	l.Debug(fmt.Sprintf(format, v...))
+	l.logf(slog.LevelDebug, format, v...)
 }
 
 func (l Logger) Infof(format string, v ...any) {
-	l.Logger.Info(fmt.Sprintf(format, v...))
+	l.logf(slog.LevelInfo, format, v...)
+}
+
+func (l Logger) Warnf(format string, v ...any) {
+	l.logf(slog.LevelWarn, format, v...)
+}
+
+func (l Logger) Error(err error, msg string, args ...any) {
+	args = append(args, "error")
+	args = append(args, err)
+	l.log(slog.LevelError, msg, args...)
 }
 
 func (l Logger) Errorf(err error, format string, v ...any) {
-	l.Logger.Error(err, fmt.Sprintf(format, v...))
+	l.log(slog.LevelError, fmt.Sprintf(format, v...), err)
 }
 
-// func (l Logger) Warn(msg string, keysAndValues ...interface{}) {
-// 	l.Logger.V(2).Info(msg, keysAndValues...)
-// }
-
-func (l Logger) Fatal(err error, msg string, keysAndValues ...interface{}) {
-	l.Logger.Error(err, msg, keysAndValues...)
+func (l Logger) Fatal(err error, msg string, args ...any) {
+	args = append(args, "error")
+	args = append(args, err)
+	l.log(slog.LevelError, msg, args...)
 	os.Exit(1)
 }
 
 func (l Logger) Fatalf(err error, format string, v ...any) {
-	l.Fatal(err, fmt.Sprintf(format, v...))
-}
-
-// Options that can be passed to NewWithOptions
-type Options struct {
-	// Name is an optional name of the logger
-	Name       string
-	TimeFormat string
-	Output     io.Writer
-	// Logger is an instance of zerolog, if nil a default logger is used
-	Logger *zerolog.Logger
-}
-
-// New returns a logr.Logger, LogSink is implemented by zerolog.
-func New() Logger {
-	return Logger{NewWithOptions(Options{})}
-}
-
-// NewWithOptions returns a logr.Logger, LogSink is implemented by zerolog.
-func NewWithOptions(opts Options) logr.Logger {
-	if opts.TimeFormat != "" {
-		zerolog.TimeFieldFormat = opts.TimeFormat
-	} else {
-		zerolog.TimeFieldFormat = timeFormat
-	}
-
-	var out io.Writer
-	if opts.Output != nil {
-		out = opts.Output
-	} else {
-		out = getOutputFormat()
-	}
-
-	if opts.Logger == nil {
-		l := zerolog.New(out).With().Timestamp().Logger()
-		opts.Logger = &l
-	}
-
-	ls := zerologr.NewLogSink(opts.Logger)
-	if zerolog.LevelFieldName == "" {
-		// Restore field removed by Zerologr
-		zerolog.LevelFieldName = "level"
-	}
-	l := logr.New(ls)
-	if opts.Name != "" {
-		l = l.WithName(opts.Name)
-	}
-	return l
-}
-
-func getOutputFormat() zerolog.ConsoleWriter {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, NoColor: false}
-	output.FormatTimestamp = func(i interface{}) string {
-		return "[" + i.(string) + "]"
-	}
-	output.FormatLevel = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf("[%-3s]", i))
-	}
-	output.FormatMessage = func(i interface{}) string {
-		_, file, line, _ := runtime.Caller(10)
-		return fmt.Sprintf("[%s:%d] => %s", filepath.Base(file), line, i)
-	}
-	return output
+	l.log(slog.LevelError, fmt.Sprintf(format, v...), err)
+	os.Exit(1)
 }
