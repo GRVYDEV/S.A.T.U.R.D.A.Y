96d0960266f1aa771759cb417180eb293f56eb55
Garrett Graves <gravesg57@gmail.com>
2023-07-02 19:45:58 +0100

    fix prompt bug

--- ttt/backends/openai/openai.go
+++ ttt/backends/openai/openai.go
@@ -3,7 +3,6 @@ package openai
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	logr "github.com/GRVYDEV/S.A.T.U.R.D.A.Y/log"
 	"github.com/GRVYDEV/S.A.T.U.R.D.A.Y/ttt/engine"
@@ -14,7 +13,7 @@ import (
 var _ engine.Generator = (*OpenAIBackend)(nil)
 var Logger = logr.New()
 
-const PREFIX = `
+const SYSTEM_MESSAGE = `
 Your name is Saturday.
 
 Saturday is a conversational, vocal, artificial intelligence assistant.
@@ -24,14 +23,6 @@ Saturday's job is to converse with humans to help them accomplish goals.
 Saturday is able to help with a wide variety of tasks from answering questions to assisting the human with creative writing.
 
 Overall Saturday is a powerful system that can help humans with a wide range of tasks and provide valuable insights as well as taking actions for the human.
-`
-
-const SUFFIX = `
-DO NOT start your response with "Assistant:"
-
-Given the following prompt respond as a friendly assistant:
-
-PROMPT: %s
 `
 
 type OpenAIBackend struct {
@@ -60,11 +51,11 @@ func (o *OpenAIBackend) Generate(prompt string) (engine.TextChunk, error) {
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleSystem,
-					Content: PREFIX,
+					Content: SYSTEM_MESSAGE,
 				},
 				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: fmt.Sprintf(SUFFIX, prompt),
+					Content: prompt,
 				},
 			},
 		},
